526272c926adce6040621a2e805ece0abe0b04bf
YassineR <yassine.rachedi@yahoo.fr>
2023-10-17 13:40:09 +0200

    Add files

---
+++ bootstrap-emr.sh
@@ -0,0 +1,13 @@
+sudo python3 -m pip install -U setuptools
+sudo python3 -m pip install -U pip
+sudo python3 -m pip install wheel
+sudo python3 -m pip install pillow
+sudo python3 -m pip install pandas
+sudo python3 -m pip install pyarrow
+sudo python3 -m pip install botocore==1.31.17
+sudo python3 -m pip install boto3==1.28.17
+sudo python3 -m pip install s3fs
+sudo python3 -m pip install fsspec
+sudo python3 -m pip install keras
+sudo python3 -m pip install soupsieve
+sudo python3 -m pip install matplotlib
